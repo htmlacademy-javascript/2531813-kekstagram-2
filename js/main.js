26ede5983579fee57ae9276e69290f39cca3b60d
Ilya <89048297214z@gmail.com>
2025-05-27 14:41:36 +0300

    openning and closing part 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 import { getPhotos } from "./photos.js";
 import { render } from "./thumbnail.js";
+import { openBigPicture } from './bigPicture.js';
+import { container } from './thumbnail.js';
 const photosData = getPhotos();
 console.log(photosData);
 
 render(photosData);
 
 
+container.addEventListener('click', (evt) => {
+  const thumbnailElement = evt.target.closest('.picture');
+  if (!thumbnailElement) return;
+
+  const index = Array.from(container.children).indexOf(thumbnailElement);
+  if (index === -1) return;
+
+  const photoData = photosData[index];
+  openBigPicture(photoData);
+});
