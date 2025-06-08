ef716519459b522f7ddd3453a0d1e4a02b5bb0a3
Ilya <89048297214z@gmail.com>
2025-06-08 20:39:55 +0300

    Truth-Dare

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import { getPhotos } from "./photos.js";
 import { render } from "./thumbnail.js";
 import { openBigPicture } from './bigPicture.js';
 import { container } from './thumbnail.js';
+import './form.js';
+import './validation.js';
 const photosData = getPhotos();
 console.log(photosData);
 
